4fe93f8c5aee1cfc0a61883d7b8de0990b104329
kothu555 <116157727+kothu555@users.noreply.github.com>
2023-10-01 01:10:40 +0630

    versions.tf

---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
+  region                   = "your aws region"
+  alias                    = "example"
+}
